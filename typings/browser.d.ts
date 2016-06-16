578664031ce7cc499f39653b39be0d5467ccb4f1
Joao Garin <joao.garin@gmail.com>
2016-06-16 20:41:38 +0200

    Upgrade to RC2

--- typings/browser.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="browser/ambient/core-js/core-js.d.ts" />
-/// <reference path="browser/ambient/zone.js/zone.js.d.ts" />
--- typings/main.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="main/ambient/core-js/core-js.d.ts" />
-/// <reference path="main/ambient/zone.js/zone.js.d.ts" />

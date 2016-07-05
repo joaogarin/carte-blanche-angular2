20f587ba5909ff77f4947ffea4fb2eb33d85c78f
Joao Garin <joao.garin@gmail.com>
2016-07-05 20:47:11 +0200

    Remove tsd debug info

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,6 +1,8 @@
+
 export default function playground(frontendData, pluginData, Component, componentPath) {
   console.log(pluginData);
   console.log(Component);
   console.log(componentPath);
   // Render components here
+  return '<h1>Hello World</h1>';
 }

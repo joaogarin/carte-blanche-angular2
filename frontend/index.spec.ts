8415a526dedc97d7eea5ec44ece90a2898d36f56
Joao Garin <joao.garin@gmail.com>
2016-07-10 17:04:04 +0200

    Added testing to the frontend

---
+++ frontend/index.spec.ts
@@ -0,0 +1,20 @@
+import {
+    iit,
+    it,
+    ddescribe,
+    describe,
+    expect,
+    async,
+    inject,
+    beforeEachProviders
+} from '@angular/core/testing';
+import {
+    TestComponentBuilder,
+    ComponentFixture
+} from '@angular/compiler/testing';
+
+describe('Frontend Index', () => {
+    it('should exist', () => {
+        expect(true).toBe(true);
+    });
+});

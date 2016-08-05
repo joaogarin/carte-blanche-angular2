d77658494a605fa345f68ac48ca7ea8e4c5ee9a5
Joao Garin <joao.garin@gmail.com>
2016-08-05 12:14:31 +0200

    Added some build improvements and update deps

---
+++ frontend/vendor.ts
@@ -0,0 +1,8 @@
+/**
+ * Vendor - Move to different file (problem with commonchunks on carte blance)
+ */
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';

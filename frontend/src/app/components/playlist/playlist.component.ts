c5354c5912160f5b3d31cdbd3c2d6bda644a1452
Joao Garin <joao.garin@gmail.com>
2016-09-04 22:09:10 +0200

    clean playlist component style

--- frontend/src/app/components/playlist/playlist.component.ts
+++ frontend/src/app/components/playlist/playlist.component.ts
@@ -2,7 +2,7 @@
  * Angular 2 decorators and services
  */
 import { Component, Input, OnChanges } from '@angular/core';
-
+import { CardComponent } from './../common/card/card.component.ts';
 import {DynamicOutlet} from './../dynamicOutlet/dynamicOutlet.component.ts';
 
 @Component({
@@ -18,9 +18,32 @@ import {DynamicOutlet} from './../dynamicOutlet/dynamicOutlet.component.ts';
         flex-direction: column;
         position: relative;
     }
+
+    .playground-card {
+        width: 90%;
+        display: -webkit-box;
+        display: flex;
+        -webkit-box-pack: center;
+        justify-content: center;
+        -webkit-box-align: center;
+        align-items: center;
+        margin: 0 auto;
+        padding: 1rem 0;
+        background: white;
+    }
+    .title {
+        font-size: 1.5em;
+        font-weight: 300;
+        margin: 0 0 0.5em 0;
+    }
   `],
     template: `<div class="wrapper">
-        <cb-dynamic-outlet [componentPath]="componentPath" [component]="component" [variationData]="variationData"></cb-dynamic-outlet>
+    <h2 class="title">{{variationData.name}}</h2>
+    <div class="playground-card">
+        <cb-card>
+            <cb-dynamic-outlet [componentPath]="componentPath" [component]="component" [variationData]="variationData"></cb-dynamic-outlet>
+        </cb-card>
+    </div>
     </div>`,
 })
 export class Playlist implements OnChanges {

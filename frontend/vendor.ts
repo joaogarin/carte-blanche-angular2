912ad9c264a5c06366afe66bfdc3a4a13bea937e
Joao Garin <joao.garin@gmail.com>
2016-09-07 23:31:21 +0200

    feat(angular): Update vendors to newer versions

--- frontend/vendor.ts
+++ frontend/vendor.ts
@@ -1,7 +1,6 @@
 /**
  * Vendor - Move to different file (problem with commonchunks on carte blance)
  */
-// Angular 2
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
 import '@angular/core';
